d17178fb83187c01793301bbcd2f0d1f5afd955d
JinchengGao-Infty <g214907378@gmail.com>
2026-04-02 15:59:39 +0800

    security: enable CSP to prevent XSS escalation
    Replace null CSP with restrictive policy. Allow 'self' for scripts,
    inline styles for antd, HTTPS connections for LLM APIs, and
    data/blob URIs for images.

--- .vite/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-ZU3UJ2BS.js";
-import "./chunk-BHRJYGXG.js";
-import "./chunk-EQCVQC35.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
